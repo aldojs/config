37693ddb9c538bbba48ba766165b26bae4201dc8
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2018-03-13 17:00:23 +0100

    Update index.d.ts
    Add documentation

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,80 @@
 
-export type Literal = { [x: string]: any }
+/**
+ * Configuration store
+ */
+export default class Store {
 
-export class Config {
-  enable (key: string): any
-  disable (key: string): any
-  has (key: string): boolean
-  merge (values: Literal): any
-  enabled (key: string): boolean
-  disabled (key: string): boolean
-  set (key: string, value: any): Config
-  get (key: string, defaults?: any): any
+  /**
+   * Enable a setting
+   * 
+   * @param key
+   */
+  enable(key: string): any;
+
+  /**
+   * Disable a setting
+   * 
+   * @param key
+   */
+  disable(key: string): any;
+
+  /**
+   * Check if a setting is defined
+   * 
+   * @param key
+   */
+  has(key: string): boolean;
+
+  /**
+   * Check if a setting is enabled
+   * 
+   * @param key
+   */
+  enabled(key: string): boolean;
+
+  /**
+   * Check if a setting is disabled
+   * 
+   * @param key
+   */
+  disabled(key: string): boolean;
+
+  /**
+   * Set a setting value
+   * 
+   * Examples:
+   * 
+   *    // simple key
+   *    config.set('foo', 123)
+   * 
+   *    // composite key
+   *    config.set('foo.bar.baz', [1, 2, 3])
+   * 
+   * @param key
+   * @param value
+   */
+  set(key: string, value: any): this;
+
+  /**
+   * Get the `key` value or `defaultValue` if not exist
+   * 
+   * Examples:
+   * 
+   *    // simple key with fallback value
+   *    config.get('foo', 'bar')
+   * 
+   *    // composite key
+   *    config.get('foo.bar.baz')
+   * 
+   * @param key
+   * @param defaultValue
+   */
+  get(key: string, defaultValue?: any): any;
+
+  /**
+   * Merge the settings with the new values
+   * 
+   * @param values
+   */
+  merge(values: { [x: string]: any }): any;
 }
