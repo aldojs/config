1f8d865d2002a3ebc2e89b85bf5ea6b462944cec
absolux <a.mouhassine@gmail.com>
2018-06-16 16:35:19 +0000

    Refactor the config store

--- src/store.ts
+++ src/store.ts
@@ -1,51 +1,50 @@
 
-import assert from 'assert'
+import is from '@sindresorhus/is'
 
-type Literal = { [x: string]: any }
+export type Literal = { [x: string]: any }
 
 /**
- * Configuration store
+ * The Configuration store class
+ * 
+ * @public
+ * @class
  */
-export default class {
-  private _data: Literal
+export class Store {
+  /**
+   * The configuration map
+   * 
+   * @private
+   */
+  private _map: Literal
 
   /**
-   * Initialize a new `Config` instance
+   * Initialize a new config store
    * 
-   * @param source
+   * @param map The source config map
+   * @constructor
+   * @public
    */
-  constructor (source: Literal = {}) {
-  	this._data = source
+  constructor (map: Literal = {}) {
+  	this._map = map
   }
 
   /**
    * Get the `key` value or `defaultValue` if not exist
    * 
-   * @param key
-   * @param defaultValue
+   * @param key The setting key name
+   * @param defaultValue The default value to return is missing
+   * @public
    */
   public get (key: string, defaultValue?: any): any {
-    var data = this._data
-    var keys = _ensure(key).split('.')
-
-    while (keys.length > 1) {
-      let field = keys.shift() as string
-
-      if (!_isPlainObject(data[field])) {
-        return defaultValue
-      }
-
-      data = data[field]
-    }
-
-    return data[keys.shift() as string] || defaultValue
+    return this._get(key) || defaultValue
   }
 
   /**
    * Set a setting value
    * 
-   * @param key
-   * @param value
+   * @param key The setting name
+   * @param value The setting value
+   * @public
    */
   public set (key: string, value: any): this {
     return this._set(key, value)
@@ -55,44 +54,51 @@ export default class {
    * Check if a setting is defined
    * 
    * @param key
+   * @public
    */
   public has (key: string): boolean {
-    return this.get(key) != null
+    return this._get(key) != null
   }
 
   /**
    * Enable a setting
    * 
    * @param key
+   * @public
    */
   public enable (key: string): this {
-    return this._setEnabled(key, true)
+    this._setEnabled(key, true)
+    return this
   }
 
   /**
    * Check if a setting is enabled
    * 
    * @param key
+   * @public
    */
   public enabled (key: string): boolean {
-    var value = this.get(key)
+    let value = this._get(key)
 
-    return (_isPlainObject(value) ? value.enabled : value) == true
+    return (is.plainObject(value) ? value['enabled'] : value) == true
   }
 
   /**
    * Disable a setting
    * 
    * @param key
+   * @public
    */
   public disable (key: string): this {
-    return this._setEnabled(key, false)
+    this._setEnabled(key, false)
+    return this
   }
 
   /**
    * Check if a setting is disabled
    * 
    * @param key
+   * @public
    */
   public disabled (key: string): boolean {
     return !this.enabled(key)
@@ -102,93 +108,85 @@ export default class {
    * Merge the settings with the new values
    * 
    * @param values
+   * @public
    */
-  public merge (values: Literal): this {
-    // TODO assert values is a plain object
-    _merge(this._data, values)
+  public merge (values: Literal | Store): this {
+    if (values instanceof Store) values = values._map
+
+    _merge(this._map, values)
+
     return this
   }
 
   /**
+   * Set `key` state
    * 
-   * 
-   * @param {String} key
-   * @param {Boolean} value
-   * @returns {Config}
+   * @param key
+   * @param value
    * @private
    */
-  private _setEnabled (key: string, value: boolean): this {
-    var old = this.get(key)
+  private _setEnabled (key: string, value: boolean) {
+    let old = this.get(key)
 
-    if (_isPlainObject(old)) {
-      key = `${key}.enabled`
-    }
+    if (is.plainObject(old)) key += '.enabled'
 
-    return this._set(key, false)
+    this._set(key, value)
   }
 
   /**
-   * Set a setting value
+   * Mutate the map
    * 
-   * @param {String} key
-   * @param {Any} value
-   * @returns {Config}
+   * @param key 
+   * @param value 
    * @private
    */
-  private _set (key: string, value: any) {
-    var data = this._data
-    var keys = _ensure(key).split('.')
+  private _set (key: string, value: any): this {
+    let data = this._map
+    let keys = key.split('.')
 
     while (keys.length > 1) {
       let field = keys.shift() as string
 
-      // If the key doesn't exist at this depth,
+      // if the key doesn't exist at this depth,
       // we'll just create a plain object to hold the value
-      if (!_isPlainObject(data[field])) {
-        data[field] = {}
-      }
+      if (!is.plainObject(data[field])) data[field] = {}
 
       data = data[field]
     }
 
-    // last field
-    data[keys.shift() as string] = value
+    data[keys[0]] = value
 
     return this
   }
-}
 
-/**
- * Ensure the configuration key is valid
- * 
- * @param {String} key
- * @returns {String}
- * @throws {AssertionError}
- * @private
- */
-function _ensure (key: string) {
-  assert(typeof key === 'string', 'The key must be a string')
-  assert(key !== '', 'The key should not be empty')
-  
-  return key
-}
+  /**
+   * Get a value from the map
+   * 
+   * @param key 
+   * @private
+   */
+  private _get (key: string): any {
+    let map = this._map
+    let keys = key.split('.')
 
-/**
- * Check if the given value is a plain object
- * 
- * @param {Any} arg
- * @returns {Boolean}
- * @private
- */
-function _isPlainObject (arg: any): boolean {
-  return arg && (Reflect.getPrototypeOf(arg) === null || Object === arg.constructor)
+    while (keys.length > 1) {
+      let field = keys.shift() as string
+
+      // we won't go that deep if the field is not an object
+      if (!is.plainObject(map[field])) return
+
+      map = map[field]
+    }
+
+    return map[keys[0]]
+  }
 }
 
 /**
  * Merge two objects
  * 
- * @param {Object} dest
- * @param {Object} source
+ * @param dest
+ * @param source
  * @private
  */
 function _merge (dest: Literal, source: Literal) {
@@ -196,13 +194,13 @@ function _merge (dest: Literal, source: Literal) {
     let value = source[field]
 
     // list
-    if (Array.isArray(dest[field])) {
+    if (is.array(dest[field])) {
       dest[field] = dest[field].concat(value)
       continue
     }
 
     // map
-    if (_isPlainObject(value) && _isPlainObject(dest[field])) {
+    if (is.plainObject(value) && is.plainObject(dest[field])) {
       _merge(dest[field], value)
       continue
     }
